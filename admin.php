bf84febb482fdc7bcbd8520f92b078017ff606f8
randisunarto <148059279+randisunarto@users.noreply.github.com>
2024-05-23 19:47:25 +0700

    membuat sign in for admin

---
+++ admin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Sign-In</title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+ <div class="wrapper">
+    <div class="container">
+        <h1>Admin Sign in</h1>
+        
+        <form class="form">
+            <input type="text" placeholder="Username">
+            <input type="password" placeholder="Password">
+            <button type="submit" id="login-button">Login</button>
+        </form>
+    </div>
+    
+    <ul class="bg-bubbles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
+</body>
+</html>
